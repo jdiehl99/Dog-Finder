71142963ad464c01d7d36cfb8313b1be92bdfe53
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-01 12:51:17 -0400

    added modal to survey results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,10 +23,6 @@ function apiStuff(app, __dirname) {
         // take user inputs and shove them into an array
         var formData = req.body;
         userAnswers.push(formData);
-        console.log("tobeat", toBeat);
-        console.log("useranswers", userAnswers);
-        // console.log("user score length",req.body.scores.length);
-        // console.log("friends score length",friends[0].scores.length);
 
         // // run through the friends array
         for (i = 0; i < friends.length; i++) {
@@ -35,33 +31,25 @@ function apiStuff(app, __dirname) {
 
 
             for (s = 0; s < friends[i].scores.length; s++) {
-                // console.log(friends[i].name + " score value q" + [s], friends[i].scores[s]);
-                // console.log("user score value q" + [s], formData.scores[s]);
-                // console.log("user scores",req.body.scores[s]);
-                // console.log("friend score",friends[i].scores[s].length);
                 var diff = 0;
                 diffval = Math.abs(formData.scores[s] - friends[i].scores[s]);
                 differenceArr.push(diffval);
             }
-            console.log(friends[i].name + "diff arr", differenceArr);
             for (d = 0; d < differenceArr.length; d++) {
                 diff += differenceArr[d];
             }
-            console.log(friends[i].name + " diff", diff);
-            console.log("tobeat", toBeat);
+            // determine if this friend is better than the current lowest score
             if (diff < toBeat) {
                 toBeat = diff;
                 bestMatch = friends[i].name;
                 bestImg = friends[i].photo;
             }
         }
-        console.log("best match", bestMatch);
         yourDog.push(bestMatch);
         yourDog.push(bestImg);
-        yourDog.push(req.body.name);
-        yourDog.push(req.body.photo);
+        yourDog.push(formData.name);
+        yourDog.push(formData.photo);
         res.json(yourDog);
-        console.log("your dog array",yourDog);
     });
 }
 
